8b7d13594f1635490418d3ef1c896c3490145315
ana-martinezal2021 <a.martinezal.2021@alumnos.urjc.es>
2026-03-14 11:02:41 +0100

    Actualización memoeria, corrección c1 y versión 1 de c2

---
+++ src/UI_ESP32/MyMeds/src/servo_test/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+
+#define SERVOMIN  150
+#define SERVOMAX  600
+
+uint16_t pos0  = SERVOMIN;
+uint16_t pos90 = (SERVOMIN + SERVOMAX) / 2;
+uint16_t pos45 = (pos0 + pos90) / 2;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Wire.begin(22,27);
+
+    pwm.begin();
+    pwm.setPWMFreq(50);
+
+    delay(1000);
+}
+
+void loop()
+{
+    Serial.println("0°");
+    pwm.setPWM(0, 0, pos0);
+    delay(2000);
+
+    Serial.println("45");
+    pwm.setPWM(0, 0, pos45);
+    delay(2000);
+
+    Serial.println("90");
+    pwm.setPWM(0, 0, pos90);
+    delay(2000);
+}
