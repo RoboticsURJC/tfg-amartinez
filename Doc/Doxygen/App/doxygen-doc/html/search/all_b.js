8f126981077e7b64a46220cccbfda7efae896d3a
ana-martinezal2021 <a.martinezal.2021@alumnos.urjc.es>
2026-07-02 20:21:15 +0200

    Correcciones memoria versión final

---
+++ Doc/Doxygen/App/doxygen-doc/html/search/all_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['notificationhelper_2ekt_0',['NotificationHelper.kt',['../NotificationHelper_8kt.html',1,'']]],
+  ['notificationscheduler_2ekt_1',['NotificationScheduler.kt',['../NotificationScheduler_8kt.html',1,'']]]
+];
---
+++ Doc/Doxygen/App/doxygen-doc/html/search/files_8.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['ic_5flauncher_2exml_0',['ic_launcher.xml',['../ic__launcher_8xml.html',1,'']]],
+  ['ic_5flauncher_5fbackground_2exml_1',['ic_launcher_background.xml',['../drawable_2ic__launcher__background_8xml.html',1,'(Global Namespace)'],['../values_2ic__launcher__background_8xml.html',1,'(Global Namespace)']]],
+  ['ic_5flauncher_5fforeground_2exml_2',['ic_launcher_foreground.xml',['../ic__launcher__foreground_8xml.html',1,'']]],
+  ['ic_5flauncher_5fround_2exml_3',['ic_launcher_round.xml',['../ic__launcher__round_8xml.html',1,'']]],
+  ['ic_5fmenu_5fcamera_2exml_4',['ic_menu_camera.xml',['../ic__menu__camera_8xml.html',1,'']]],
+  ['ic_5fmenu_5fgallery_2exml_5',['ic_menu_gallery.xml',['../ic__menu__gallery_8xml.html',1,'']]],
+  ['ic_5fmenu_5fslideshow_2exml_6',['ic_menu_slideshow.xml',['../ic__menu__slideshow_8xml.html',1,'']]],
+  ['ic_5fnotification_2exml_7',['ic_notification.xml',['../ic__notification_8xml.html',1,'']]],
+  ['ic_5fqr_5fcode_2exml_8',['ic_qr_code.xml',['../ic__qr__code_8xml.html',1,'']]],
+  ['item_5fmedicine_2exml_9',['item_medicine.xml',['../item__medicine_8xml.html',1,'']]],
+  ['item_5fmedicine_5fsimple_2exml_10',['item_medicine_simple.xml',['../item__medicine__simple_8xml.html',1,'']]],
+  ['item_5fpulse_5fhistory_2exml_11',['item_pulse_history.xml',['../item__pulse__history_8xml.html',1,'']]],
+  ['item_5ftake_2exml_12',['item_take.xml',['../item__take_8xml.html',1,'']]],
+  ['item_5ftoday_5ftake_2exml_13',['item_today_take.xml',['../item__today__take_8xml.html',1,'']]]
+];
